65d10679e50a9d346517b943e312c50fdd4fb652
mazor432 <przemex6@gmail.com>
2019-11-17 20:08:57 +0100

    Applicaton base

---
+++ src/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledApp = styled.div`
+    max-width: 100vw;
+    height: 100vh;
+    background-color: #EBEFF2;
+    overflow: auto;
+`
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+`
+export const StyledTitle = styled.h1`
+    color: black;
+    text-align: center;
+    font-size: 3rem;
+    padding-top: 2rem;
+    font-family: 'Roboto Mono', monospace;
+    `
