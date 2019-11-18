7984288a3e51b5642b700aca402b25ce6c4fc01c
mazor432 <przemex6@gmail.com>
2019-11-18 23:04:57 +0100

    Add style to input and hints

--- src/Components/CountryInput/style.js
+++ src/Components/CountryInput/style.js
@@ -1,12 +1,50 @@
 import styled from 'styled-components'
 
-export const StyledInput = styled.input`
-    width: 30%;
-    height: 100%;
-}`
-
 export const HintWrapper = styled.ul`
+    width: 40%;
+    position: relative;
+    background-color: white;
+    box-shadow: 0px 0px 40px #B7B7B7;
+    z-index: 100;
+    list-style-type: none;
+    top:10px;
+    margin-block:0; 
+    margin-inline:0;
+    padding-block:0; 
+    padding-inline:0; 
 `
 
 export const Hint = styled.li`
+    width: 100%;
+    height: 35px;
+    font-weight: 300;
+    font-size: 20px;
+    cursor: pointer;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    &:hover {
+        background-color: #f2f2f2;
+        font-weight: 400;
+    }
+    &:first-child {
+        padding-top: 5px;
+    }
+`
+
+export const StyledInput = styled.input`
+    width: 40%;
+    height: 100%;
+    padding: 20px;
+    border: 0;
+    font-size: 25px;
+    font-weight: 300;
+    border-bottom: 3px solid #C8CCD4;
+    transition: all .150s ease;
+    background-color: white;
+    text-align: center;
+    &:focus {
+        outline: none
+        box-shadow: 0px 0px 40px #B7B7B7;
+    }
 `
