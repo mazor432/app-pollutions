ab28a4443b9abbc857b080b5c1c3495b6ebc2791
mazor432 <przemex6@gmail.com>
2019-11-19 23:41:45 +0100

    Change to 10 cities display

--- src/API/openaq.js
+++ src/API/openaq.js
@@ -4,7 +4,7 @@ export const getCities = (inputVal) => {
   return axios
     .get( `https://api.openaq.org/v1/latest?country=${inputVal}&order_by=measurements[0].value&sort=desc&parameter=pm25`)
         .then(response => {
-            const truncatedData = [...new Set(response.data.results.map(item => item.city))].slice(0, 9)
+            const truncatedData = [...new Set(response.data.results.map(item => item.city))].slice(0, 10)
             return truncatedData;
         })
         .catch(function(error) {
