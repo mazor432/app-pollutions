71b5d21b38dacf52daed1e3c237b5b0c7bba1590
Przemek Wnuk <mazor432@gmail.com>
2019-12-29 17:58:29 +0000

    Updates

---
+++ precache-manifest.e72cdc4d98e21a94296e34b04e6ff84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2583b27576ac7eb870bdffd22099eb7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "369673cf12544ccc7361",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d931f451c438118349c0",
+    "url": "/static/js/2.8e543ed6.chunk.js"
+  },
+  {
+    "revision": "369673cf12544ccc7361",
+    "url": "/static/js/main.99c0d756.chunk.js"
+  },
+  {
+    "revision": "31f10273423be7ac6a8a",
+    "url": "/static/js/runtime-main.45c65bc8.js"
+  }
+]);
